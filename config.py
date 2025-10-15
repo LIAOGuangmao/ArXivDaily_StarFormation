663e23b874ef59a5f9d072f19e2b3b5a2a6899a9
wuyou <115723539+LIAOGuangmao@users.noreply.github.com>
2025-10-15 21:48:06 +0800

    Update config.py
    add new keywords to search

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ from __future__ import unicode_literals
 USERNAME = 'olozhika'
 
 # The repository to add this issue to
-REPO_OWNER = 'olozhika'
+REPO_OWNER = 'LIAOGuangmao'
 REPO_NAME = 'ArXivDaily_StarFormation'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["star formation", "star-forming", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF"]
+KEYWORD_LIST = ["star formation", "star-forming", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF", "initial conditions", "IRDC"]
 # Keywords to exclude
 KEYWORD_EX_LIST = ["galaxies", "galaxy cluster", " AGN ", "standard candle", "X-ray binar", "solar corona"]
 # Note that the 'Keywords' above are actually searched in the abstract instead of the real keyword section. 
